26451f8b7a7559387d595c3e2e137c733fe6edff
kissemisse <gustav.liden99@gmail.com>
2020-06-26 19:34:36 +0200

    pick up

--- Parallel/Assets/Scripts/Player.cs
+++ Parallel/Assets/Scripts/Player.cs
@@ -7,10 +7,17 @@ public class Player : MonoBehaviour {
     bool inDim1 = true;
 
     [SerializeField]
-    Material dim1mat, dim2mat;
+    int maxHealth;
+
+    [SerializeField]
+    int maxArmour;
+
+    int health;
+    int armour;
 
     void Start() {
-        
+        health = maxHealth;
+        armour = maxArmour;
     }
 
     void Update() {
@@ -30,17 +37,41 @@ public class Player : MonoBehaviour {
 
             foreach(GameObject obj in GameObject.FindGameObjectsWithTag("Changable Objects")) {
                 // object that change depending on dimensions
+                obj.GetComponent<MaterialChange>().ChangeMaterial(true);
             }
 
             foreach(GameObject enemy in GameObject.FindGameObjectsWithTag("Enemy")) {
                 // enemies
-
+                //enemy.GetComponent<>().(true);
             }
 
             inDim1 = false;
 
         } else { // change to dim1
 
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        tag = collision.gameObject.transform.tag;
+        switch(tag) {
+            case "Armour":
+                if(armour < maxArmour) {
+                    armour += collision.gameObject.GetComponent<PickUp>().Value;
+                    if(armour > maxArmour) armour = maxArmour;
+                    Destroy(collision.gameObject);
+                }
+                break;
+            case "Health":
+                if(health < maxHealth) {
+                    health += collision.gameObject.GetComponent<PickUp>().Value;
+                    if(health > maxHealth) health = maxHealth;
+                    Destroy(collision.gameObject);
+                }
+                break;
+
+            default:
+                break;
         }
     }
 }
