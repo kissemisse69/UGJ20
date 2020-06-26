093d4f4d060393c286fede75bf3740571391993b
kissemisse <gustav.liden99@gmail.com>
2020-06-26 18:57:01 +0200

    worked on dimension shift

--- Parallel/Assets/Scripts/MaterialChange.cs
+++ Parallel/Assets/Scripts/MaterialChange.cs
@@ -3,18 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MaterialChange : MonoBehaviour {
-    
 
+    [SerializeField]
+    Material dim1, dim2;
 
-    void Start() {
-        
-    }
-
-    void Update() {
-        //test remove later
-    }
-
-    void ChangeMaterial() {
-
+    public void ChangeMaterial(bool changeToDim2) {
+        if(changeToDim2) { // change to dim 2
+            GetComponent<MeshRenderer>().material = dim2;
+        } else { // change to dim 1
+            GetComponent<MeshRenderer>().material = dim1;
+        }
     }
 }
---
+++ Parallel/Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    bool inDim1 = true;
+
+    [SerializeField]
+    Material dim1mat, dim2mat;
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+
+        if(Input.GetKeyDown(KeyCode.LeftShift)) {
+            ChangeDimension();
+        }
+    }
+
+    void ChangeDimension() {
+        if(inDim1) { // change to dim2
+            foreach(GameObject obj in GameObject.FindGameObjectsWithTag("Static Objects")) {
+                // static objects like ground and walls
+                obj.GetComponent<MaterialChange>().ChangeMaterial(true);
+            }
+
+            foreach(GameObject obj in GameObject.FindGameObjectsWithTag("Changable Objects")) {
+                // object that change depending on dimensions
+            }
+
+            foreach(GameObject enemy in GameObject.FindGameObjectsWithTag("Enemy")) {
+                // enemies
+
+            }
+
+            inDim1 = false;
+
+        } else { // change to dim1
+
+        }
+    }
+}
