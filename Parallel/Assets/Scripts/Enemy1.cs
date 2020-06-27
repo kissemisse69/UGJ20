ecc534232792d9fd6e8a1b3b37980ddfd949333b
kissemisse <gustav.liden99@gmail.com>
2020-06-27 17:51:39 +0200

    Update Enemy1.cs

--- Parallel/Assets/Scripts/Enemy1.cs
+++ Parallel/Assets/Scripts/Enemy1.cs
@@ -1,10 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy1 : MonoBehaviour {
-    
 
+    [SerializeField]
+    float speed;
+    [SerializeField]
+    int cooldown, hp;
+
+    bool mode1 = true;
+    bool alive = true;
+
+    [SerializeField]
+    GameObject projectilePrefab;
+
+    // components
+    SpriteRenderer _spriteRenderer;
+    Animator _ani;
+    NavMeshAgent _agent;
+
+    // ai stuff
 
     void Start() {
         
